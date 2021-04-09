dcd407f3cd5f73c2a47024726c4b8eda6f8065d6
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-09 14:34:00 -0400

    All Methods Work

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        public static int Add(int x, int y)
+        {
+            int sum = x + y;
+            return sum;
+        }
+
+        public static double Add(double x, double y)
+        {
+            double decicmal = x + y;
+            return decicmal;
+        }
+
+        public static string Add(int x, int y, bool isTrue)
+        {
+            var sum = x + y;
+            if (isTrue == true)
+            {
+                
+                if (sum == 1)
+                {
+                    return sum + " dollar";
+                }
+                else
+                {
+                    return sum + " dollars";
+                }
+            }
+            else
+                return $"{sum}";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
